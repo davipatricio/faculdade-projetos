6049647f6fa873fcea23b5b12cd4aa0f14ea3022
Davi Patricio <davipatricio2006@gmail.com>
2024-08-28 13:22:29 -0300

    refactor olimipadas

--- 2-semestre/python/olimpiadas/main.py
+++ 2-semestre/python/olimpiadas/main.py
@@ -1,60 +1,46 @@
+#!/usr/bin/env python3
+
 import random
 
 def gerar_cpf():
-	return "".join([str(random.randint(0, 9)) for _ in range(9)])
+  return "".join([str(random.randint(0, 9)) for _ in range(9)])
 
-def gerar_subconjunto(conjunto):
-	return set(random.sample(list(conjunto), int(random.random() * 100)))
+def gerar_subconjunto(conjunto: set[str]) -> set[str]:
+  return set(random.sample(list(conjunto), int(random.random() * 100)))
 
+cpfs = set(gerar_cpf() for _ in range(100))
+total = len(cpfs)
 
-cpfs_gerados = set()
-
-while len(cpfs_gerados) < 100:
-	cpfs_gerados.add(gerar_cpf())
-
-assistiu = {   
-  "judo": gerar_subconjunto(cpfs_gerados),
-  "volei_de_praia": gerar_subconjunto(cpfs_gerados),
-  "ginastica": gerar_subconjunto(cpfs_gerados),
-  "surf": gerar_subconjunto(cpfs_gerados)
+publico = {
+  "judo": gerar_subconjunto(cpfs),
+  "volei": gerar_subconjunto(cpfs),
+  "ginastica": gerar_subconjunto(cpfs),
+  "surf": gerar_subconjunto(cpfs)
 }
 
-probabilidades = {
-	"judo_ou_surfe": 0,
-	"ao_menos_dois_esportes": 0,
-	"todos_esportes": 0,
-	"nenhum_esporte": 0
-}
+def calc_probabilidades(judo: set[str], volei: set[str], ginastica: set[str], surf: set[str]) -> dict[str, float]:
+    judo_ou_surfe = judo | surf
+    prob_judo_ou_surfe = len(judo_ou_surfe) / total
+    
+    pelo_menos_dois = (judo & volei) | (judo & ginastica) | (judo & surf) | (volei & ginastica) | (volei & surf) | (ginastica & surf)
+    prob_pelo_menos_dois = len(pelo_menos_dois) / total
 
-def ao_menos_dois_esportes():
-  pessoas_ao_menos_dois = set()
-
-  for cpf in cpfs_gerados:
-    esportes_assistidos = {
-      cpf in assistiu["judo"],
-      cpf in assistiu["volei_de_praia"],
-      cpf in assistiu["ginastica"],
-      cpf in assistiu["surf"]
+    todos_os_esportes = judo & volei & ginastica & surf
+    prob_todos_os_esportes = len(todos_os_esportes) / total
+    
+    nenhum_esporte = cpfs - (judo | volei | ginastica | surf)
+    prob_nenhum_esporte = len(nenhum_esporte) / total
+    
+    return {
+        "prob_judo_ou_surfe": prob_judo_ou_surfe,
+        "prob_pelo_menos_dois": prob_pelo_menos_dois,
+        "prob_todos_os_esportes": prob_todos_os_esportes,
+        "prob_nenhum_esporte": prob_nenhum_esporte
     }
 
-    if sum(esportes_assistidos) >= 2:
-      pessoas_ao_menos_dois.add(cpf)
-  
-  return pessoas_ao_menos_dois
+probabilidades = calc_probabilidades(publico["judo"], publico["volei"], publico["ginastica"], publico["surf"])
 
-def calc_probabilidades() -> dict[str, float]:
-  probabilidades["judo_ou_surfe"] = len(assistiu["judo"] | assistiu["surf"]) / len(cpfs_gerados)
-  probabilidades["ao_menos_dois_esportes"] = len(ao_menos_dois_esportes()) / len(cpfs_gerados)
-  probabilidades["todos_esportes"] = len(assistiu["judo"] & assistiu["volei_de_praia"] & assistiu["ginastica"] & assistiu["urf"]) / len(cpfs_gerados)
-  probabilidades["nenhum_esporte"] = len(cpfs_gerados - assistiu["judo"] - assistiu["volei_de_praia"] - assistiu["ginastica"] - assistiu["surf"])
-	
-  return probabilidades
-
-probabilidades = calc_probabilidades()
-
-print(f"Total de pessoas pesquisadas: {len(cpfs_gerados)}")
-print(f"Probabilidade de que uma pessoa sorteada aleatoriamente tenha assistido Judô ou Surfe: {probabilidades['judo_ou_surfe']}")
-print(f"Probabilidade de que uma pessoa sorteada aleatoriamente tenha assistido pelo menos dois esportes: {probabilidades['ao_menos_dois_esportes']}")
-print(f"Probabilidade de que uma pessoa sorteada aleatoriamente tenha assistido todos os esportes: {probabilidades['todos_esportes']}")
-print(f"Probabilidade de que uma pessoa sorteada aleatoriamente não tenha assistido nenhum esporte: {probabilidades['nenhum_esporte']}")
-
+print("Probabilidade de ter assistido judô ou surfe:", probabilidades["prob_judo_ou_surfe"] * 100)
+print("Probabilidade de ter assistido pelo menos dois esportes:", probabilidades["prob_pelo_menos_dois"] * 100)
+print("Probabilidade de ter assistido a todos os esportes:", probabilidades["prob_todos_os_esportes"] * 100)
+print("Probabilidade de não ter assistido a nenhum esporte:", probabilidades["prob_nenhum_esporte"] * 100)
